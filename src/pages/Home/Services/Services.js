070d76e8b6b6d03361e25e54ba4f9ebc60bd9ebe
Bryan <bry@payloro.com>
2023-01-04 17:00:24 +0800

    add image

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { servicesData } from "../../../utils/helper";
-import { motion } from "framer-motion";
 
 const Services = () => {
   return (
     <Box className="bg-primary h-auto">
       <Box className="container mx-auto flex flex-col justify-center items-center text-center py-12">
-        <h3 className="text-3xl lg:text-5xl text-black font-avenirroman uppercase py-10">
+        <h3 className="text-3xl lg:text-5xl text-black font-avenirroman capitalize py-10">
           Services
         </h3>
         <Box className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-x-12 gap-y-20 md:px-0 px-8 py-8">
           {servicesData.map((item, index) => {
             const { title, description, icon } = item;
             return (
-              <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ duration: 1 }}
+              <Box
                 key={index}
                 className="flex flex-col items-center text-center gap-2 px-6 border border-gray-300 py-6 rounded-xl hover:border-primaryRed duration-300"
               >
@@ -29,7 +24,7 @@ const Services = () => {
                 />
                 <h3 className="text-2xl font-avenirheavy">{title}</h3>
                 <p className="text-md font-avenirlight">{description}</p>
-              </motion.div>
+              </Box>
             );
           })}
         </Box>
